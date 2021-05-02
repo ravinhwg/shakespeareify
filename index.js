fca4d5bbb1ed3456f41c6a40774d077c286dfd22
ravinhwg <ravinhwg@gmail.com>
2021-05-02 21:06:14 +0530

    Stop infinite reply loop.

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 require('dotenv').config()
-const Twit =  require('twit')
-const wordList =  require('./wordList.js')
+const Twit = require('twit')
+const wordList = require('./wordList.js')
 const T = new Twit({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     access_token: process.env.access_token,
     access_token_secret: process.env.access_token_secret
 })
-const stream = T.stream("statuses/filter",{track: '@shakespeareify'});
+const stream = T.stream("statuses/filter", { track: '@shakespeareify' });
 
 stream.on('tweet', grabMention);
 
-function grabMention(tweet){
+function grabMention(tweet) {
     // Check if the bot is mentioned inside the tweet text. Or don't do anything
-    if(!tweet.text.includes('@shakespeareify')){
-        return
-    }
+    if (tweet.text === '@shakespeareify') {
+        // Check if the tweet the bot is mentioned is a reply tweet
+        const isReply = tweet.in_reply_to_status_id_str !== null;
+        if (isReply) {
+            // Load the tweet above the mentioned tweet,
+            T.get('statuses/show', { id: tweet.in_reply_to_status_id_str }, (err, data, response) => {
+                // Chek if the tweet is in English if not reject and Post saying the bot cannot handle it
+                if (data.lang === 'en') {
+                    mutateTweet(data, tweet)
+                } else {
+                    // Say that the tweet is not written in English!
+                    T.post('statuses/update', {
+                        status: `Oh! @${tweet.user.screen_name}, looks like thy tweet is not writ in English `,
+                        in_reply_to_status_id: tweet.id_str
+                    })
+                }
+            })
 
-    // Check if the tweet the bot is mentioned is a reply tweet
-    const isReply = tweet.in_reply_to_status_id_str !== null;
-    if(isReply){
-     // Load the tweet above the mentioned tweet,
-     T.get('statuses/show', {id: tweet.in_reply_to_status_id_str}, (err, data, response) => {
-         // Chek if the tweet is in English if not reject and Post saying the bot cannot handle it
-         if(data.lang === 'en'){
-            mutateTweet(data, tweet)
-         }else{
-            // Say that the tweet is not written in English!
+        } else {
+            // Tell the user to not to bother me by tagging me on main tweets
             T.post('statuses/update', {
-                status: `Oh! @${tweet.user.screen_name}, looks like thy tweet is not writ in English `,
+                status: `Valorous morrow to thee, @${tweet.user.screen_name}! Thither is nothing up thither. Calleth me in a thread.`,
                 in_reply_to_status_id: tweet.id_str
             })
-         }
-     })
-
-    }else{
-    // Tell the user to not to bother me by tagging me on main tweets
-        T.post('statuses/update', {
-            status: `Valorous morrow to thee, @${tweet.user.screen_name}! Thither is nothing up thither. Calleth me in a thread.`,
-            in_reply_to_status_id: tweet.id_str
-        })
+        }
     }
 }
 
-function mutateTweet(data, tweet){
+function mutateTweet(data, tweet) {
     // Mutating the tweet into archiac english here.
     const tweetToMutate = JSON.parse(JSON.stringify(data)).text
     const mutateArray = tweetToMutate.split(' ')
@@ -51,7 +49,7 @@ function mutateTweet(data, tweet){
     mutateArray.forEach((item, index) => {
         // for each word. Go throught the wordlist
         wordList.forEach((listWord, listIndex) => {
-            if(item.toUpperCase().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ") === listWord.modern_word.toUpperCase()){
+            if (item.toUpperCase().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ") === listWord.modern_word.toUpperCase()) {
                 mutateArray[index] = listWord.old_word
             }
         })
